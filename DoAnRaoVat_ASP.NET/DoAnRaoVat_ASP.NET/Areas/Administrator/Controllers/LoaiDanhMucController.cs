91d12e3691eff6d096b4d9591f9e87debc5d4296
QuyPhi <QuyPhi@NONAME>
2017-05-23 15:52:56 +0700

    Do an mới tạo

---
+++ DoAnRaoVat_ASP.NET/DoAnRaoVat_ASP.NET/Areas/Administrator/Controllers/LoaiDanhMucController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnRaoVat_ASP.NET.Areas.Administrator.Controllers
+{
+    public class LoaiDanhMucController : Controller
+    {
+        // GET: Administrator/LoaiDanhMuc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DoAnRaoVat_ASP.NET/DoAnRaoVat_ASP.NET/Areas/Administrator/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnRaoVat_ASP.NET.Areas.Administrator.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Administrator/Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
